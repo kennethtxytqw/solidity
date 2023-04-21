ba5ce6695c454041a97ed16379b156522d370b65
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-04-21 16:21:17 +0200

    [TMP] msize tests won't pass without a way to disable Yul optimizer

--- test/libsolidity/syntaxTests/inlineAssembly/use_msize_without_optimizer.sol
+++ test/libsolidity/syntaxTests/inlineAssembly/use_msize_without_optimizer.sol
@@ -8,3 +8,4 @@ contract C {
 // ====
 // optimize-yul: false
 // ----
+// SyntaxError 6553: (52-101): The msize instruction cannot be used when the Yul optimizer is activated because it can change its semantics. Either disable the Yul optimizer or do not use the instruction.
